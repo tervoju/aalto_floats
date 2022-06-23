b76c80e0a917621e34df7b89fcea107e3369ca81
Juha Tervo <jutervo@outlook.com>
2022-06-23 10:17:58 +0300

    hiding the azure keys

--- AaltoFloatsEdge/modules/hydromodule/refe.c
+++ AaltoFloatsEdge/modules/hydromodule/refe.c
@@ -107,10 +107,18 @@ int main(int argc, char *argv[])
     int samples;
     int pause;
 
-    if (argc > 1) samples = atoi(argv[1]); else samples = 2000000;
-    if (argc > 2) pause = atoi(argv[2]); else pause = 0;
+    if (argc > 1) 
+        samples = atoi(argv[1]); 
+    else 
+        samples = 2000000;
+    
+    if (argc > 2) 
+        pause = atoi(argv[2]); 
+    else 
+        pause = 0;
 
-    if (gpioInitialise() < 0) return 1;
+    if (gpioInitialise() < 0) 
+        return 1;
 
     // Need to set GPIO as outputs otherwise wave will have no effect.
 
@@ -279,11 +287,13 @@ int main(int argc, char *argv[])
     
     FILE *fp = NULL;
     fp = fopen(filename, "w+");
+
     if (!fp)
       perror("fopen");
     
     // Print array to file
-    for(i=0; i<samples; i++) {
+    for(i=0; i<samples; i++) 
+    {
         fprintf(fp, "%d, %d, %d, %d, %d \n", 
             data[i][0], data[i][1], data[i][2], data[i][3], data[i][4]);
     }
@@ -292,7 +302,8 @@ int main(int argc, char *argv[])
 
     printf("Sampling done, filename: %s ", filename);
 
-    if (pause) time_sleep(pause);
+    if (pause) 
+        time_sleep(pause);
 
     gpioTerminate();
     free(data);
